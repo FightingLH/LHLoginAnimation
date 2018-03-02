8c0d3322f2b014dc1b5a3013f1ddacd78865c65f
lh <feeyo@feeyodeMac-mini.local>
2018-03-02 09:46:02 +0800

    login test

---
+++ LHLoginAnimation/LHLoginAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LHLoginAnimation
+//
+//  Created by  on 2018/3/2.
+//  Copyright © 2018年 lihuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LHLoginAnimation/LHLoginAnimation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LHLoginAnimation
+//
+//  Created by  on 2018/3/2.
+//  Copyright © 2018年 lihuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
